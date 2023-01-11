7f4f367c27577c61c579bd8e5eba0c28b2b6c6b9
yeopseung <108778451+YeopSeung04@users.noreply.github.com>
2023-01-11 16:24:00 +0900

    refactor: update README and LICENSE

--- index.py
+++ index.py
@@ -6,4 +6,4 @@ browser.get('http://naver.com/')
 time.sleep(10)
 browser.get('https://google.com/')
 time.sleep(10)
-# browser.implicitly_wait(10);
+# browser.implicitly_wait(10);VERSION = '0.0.18'  # temp
